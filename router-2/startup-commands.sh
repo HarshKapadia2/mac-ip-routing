dfc824140b9b3527893b0781ea93b9d3bbacc21c
Harsh Kapadia <harshgkapadia@gmail.com>
2023-03-25 19:38:24 -0400

    :truck: update: Correct router 2 & 3 IP routing rules to match topology

--- router-2/startup-commands.sh
+++ router-2/startup-commands.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # IP table routing rule
-ip route add 10.0.1.0/24 via 10.0.4.2 # To Router 1
-ip route add 10.0.3.0/24 via 10.0.4.4 # To Router 3
+ip route add 10.0.0.0/16 via 10.0.4.2 # To Router 1
 
 # Run the HTTPd server to be able to serve data
 # Also makes sure that the container does not exit
--- router-3/startup-commands.sh
+++ router-3/startup-commands.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # IP table routing rule
-ip route add 10.0.1.0/24 via 10.0.4.2 # To Router 1
-ip route add 10.0.2.0/24 via 10.0.4.3 # To Router 2
+ip route add 10.0.0.0/16 via 10.0.4.2 # To Router 1
 
 # Run the HTTPd server to be able to serve data
 # Also makes sure that the container does not exit
